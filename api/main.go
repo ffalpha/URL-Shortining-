eb3e7d72489286b09fe971a282242d9ccf575e50
ffalpha <kalanam217@gmail.com>
2022-05-01 16:24:21 +0530

    Add gitignore

--- api/main.go
+++ api/main.go
@@ -1,1 +1,16 @@
 pacakge main
+
+import(
+	"github.com/gofiber/fiver/v2"
+)
+
+
+func setupRoutes(app * fiber.App){
+	app.get("/:url",routes.ResolveURL)
+	app.Post("/api/v1",routes.ShortenURL)
+}
+
+func main(){
+   err:= godotenv.Load()
+   app := fiber.New()
+}
